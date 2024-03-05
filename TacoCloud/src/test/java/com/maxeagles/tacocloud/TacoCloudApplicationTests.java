bb7b54a4b511fb99f50d5bbd65a8a44344354704
Max <maximumendless@yandex.ru>
2024-03-05 09:21:55 +0300

    Day 1

---
+++ TacoCloud/src/test/java/com/maxeagles/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxeagles.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
