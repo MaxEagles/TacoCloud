292edee3121b40c6bc9e8a45ae95d5495c3b7892
Max <maximumendless@yandex.ru>
2024-03-06 09:45:20 +0300

    Day 2

--- TacoCloud/src/main/java/com/maxeagles/tacocloud/DesignTacoController.java
+++ TacoCloud/src/main/java/com/maxeagles/tacocloud/DesignTacoController.java
@@ -1,11 +1,12 @@
 package com.maxeagles.tacocloud;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.maxeagles.tacocloud.Ingredient.Type;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,22 +23,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+	
+	private final IngredientRepository ingredientRepo;
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
 
 	@ModelAttribute
 	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-			new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-			new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-			new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-			new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-			new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-			new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-			new Ingredient("CHED", "Cheddar", Type.CHEESE),
-			new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-			new Ingredient("SLSA", "Salsa", Type.SAUCE),
-			new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-		);
-		
+		List<Ingredient> ingredients = (List<Ingredient>) ingredientRepo.findAll();		
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
@@ -59,7 +56,11 @@ public class DesignTacoController {
 	}
 	
 	@PostMapping
-	public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+	public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		
 		tacoOrder.addTaco(taco);
 		log.info("Processing taco: {}", taco);
 		
